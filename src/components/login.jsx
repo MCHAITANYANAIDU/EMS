a43f2e2971871c9354b09a5a3f8755bbfbe608fd
MCHAITANYANAIDU <mchaitanyaprasad65@gmail.com>
2024-11-29 08:35:58 +0530

    new

--- src/components/login.jsx
+++ src/components/login.jsx
@@ -1,33 +1,51 @@
-import React from 'react'
-import   "./style.css"
-
-
-
-
+import React from "react";
+import "./style.css";
 
 export const Login = () => {
   return (
-    <div className='d-flex justify-content-center align-items-center vh-100 Logininpage  '>
-        <div className='p-3 rounded w-25 border login form'>
-            <h2>Login Page</h2>
-            <form>
-                <div>
-                <label htmlFor='email'><strong>Email:</strong></label>
-                <input type='email'  name='email' autoComplete='off' placeholder='Enter ur Email' 
-                className="form.control rounded-0"/>
-                </div>
-                <div>
-                <label htmlFor='Password'><strong>Password:</strong></label>
-                <input type='Password'  name='Password'  placeholder='Enter ur Password' 
-                className="form.control rounded-0"/>
-                </div>
-                <button className='btn btn-success w-100 rounded-0'>Submit</button>
-                
-            </form>
-        </div>
+    <div className="d-flex justify-content-center align-items-center vh-100 login-page">
+      <div className="p-4 rounded w-25 border login-form">
+        <h2 className="text-center mb-4">Login Page</h2>
+        <form>
+          {/* Email Input */}
+          <div className="mb-3">
+            <label htmlFor="email" className="form-label">
+              <strong>Email:</strong>
+            </label>
+            <input
+              type="email"
+              id="email"
+              name="email"
+              autoComplete="off"
+              placeholder="Enter your email"
+              className="form-control rounded-0"
+              required
+            />
+          </div>
+
+          {/* Password Input */}
+          <div className="mb-3">
+            <label htmlFor="password" className="form-label">
+              <strong>Password:</strong>
+            </label>
+            <input
+              type="password"
+              id="password"
+              name="password"
+              placeholder="Enter your password"
+              className="form-control rounded-0"
+              required
+            />
+          </div>
+
+          {/* Submit Button */}
+          <button type="submit" className="btn btn-success w-100 rounded-0">
+            Submit
+          </button>
+        </form>
+      </div>
     </div>
-  )
-}
+  );
+};
 
 export default Login;
-
