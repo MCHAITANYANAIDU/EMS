50bb2d72f3fe6db2fc20f71a0f8bef7d20f2bbd3
MCHAITANYANAIDU <mchaitanyaprasad65@gmail.com>
2024-11-30 18:32:26 +0530

    updated

--- src/components/AddEmployee.jsx
+++ src/components/AddEmployee.jsx
@@ -1,3 +1,5 @@
+/* eslint-disable react/no-unknown-property */
+/* eslint-disable react/jsx-key */
 import axios from "axios";
 import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
